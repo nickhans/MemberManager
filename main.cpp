4c145db8869a3ed2e12a051c16ff50fdb42e5a8b
Nick Hanselman <nickhanselman@icloud.com>
2018-05-29 11:17:49 -0500

    created in out format for main

--- main.cpp
+++ main.cpp
@@ -7,14 +7,60 @@ void clearScreen() {
 }
 
 void displayInstructions() {
-  std::cout << "Welcome to Member Manager!\n"
-  "Choose a number from the following:\n"
+  std::cout << "Welcome to Member Manager!" << std::endl;
+  std::cout << "Main Menu" << std::endl;
+  std::cout << "Choose a number from the following:\n"
   "1. Add Member\n"
   "2. Remove Member\n"
   "3. View Member Info\n"
   "4. Edit Member Info\n"
   "5. In/Out Menu\n"
   "0. Quit\n";
+}
+
+void displayInOutInstructions() {
+  std::cout << "Choose a number from the following:\n"
+  "1. Set Individual Member In/Out Status\n"
+  "2. Set All Members In\n"
+  "3. Set All Members Out\n"
+  "4. View In Members\n"
+  "5. View Out Members\n"
+  "0. Return to Main Menu\n";
+}
+
+bool inOutMenu() {
+  displayInOutInstructions();
+  std::string input;
+  std::cout << "> ";
+  std::cin >> input;
+  if (!input.compare("1")) {
+    clearScreen();
+    std::cout << "Set Member In/Out Status" << std::endl;
+
+    clearScreen();
+  } else if (!input.compare("2")) {
+    clearScreen();
+    std::cout << "All Members In" << std::endl;
+    clearScreen();
+  } else if (!input.compare("3")) {
+    clearScreen();
+    std::cout << "All Members Out" << std::endl;
+    clearScreen();
+  } else if (!input.compare("4")) {
+    clearScreen();
+    std::cout << "In Members" << std::endl;
+    clearScreen();
+  } else if (!input.compare("5")) {
+    clearScreen();
+    std::cout << "Out Members" << std::endl;
+    clearScreen();
+  } else if (!input.compare("0")) {
+    std::cout << "Returning to main menu..." << std::endl;
+    return false;
+  } else {
+    std::cout << "\nInvalid Option\n" << std::endl;
+  }
+  return true;
 }
 
 bool inputAndExecute() {
@@ -45,6 +91,7 @@ bool inputAndExecute() {
   } else if (!input.compare("5")) {
     clearScreen();
     std::cout << "In/Out Menu" << std::endl;
+    while(inOutMenu());
     clearScreen();
   } else if (!input.compare("0")) { 
     std::cout << "Quitting..." << std::endl;
